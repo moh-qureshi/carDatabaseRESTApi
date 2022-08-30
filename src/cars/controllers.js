3e9858050d18b2325ad38da00a4e26ff692c6efd
moh-qureshi <h-qureshi95@hotmail.co.uk>
2022-08-30 14:31:05 +0100

    heroku commit

--- src/cars/controllers.js
+++ src/cars/controllers.js
@@ -17,8 +17,8 @@ exports.addVehicle = async (req, res) =>{
 exports.getAllMakes = async (req, res) => {
     try {
         const makes = await Vehicle.find()
-        console.log(req.body.makes)
-        res.send(req.body.makes)
+        console.log(makes)
+        res.send(makes)
     } catch (error) {
         console.log(error);
         res.send({ error })
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("./db/connection");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
 const vehicleRouter = require("./cars/routes");
 
 app.use(express.json());
+app.use(cors())
 app.use(vehicleRouter);
 
 app.listen(port, () =>{
